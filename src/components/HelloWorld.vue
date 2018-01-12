71a3850277deab35192fa3ce5986c53f29f4b25a
曹思源 <ll900618@163.com>
2018-01-12 23:56:24 +0800

    change

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,23 +1,8 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
-    <vue-context-menu>asdasdasd</vue-context-menu>
+    <div style="width: 30%; height:300px; margin:0 auto;">
+      <vue-context-menu>asdasdasd</vue-context-menu>
+    </div>
   </div>
 </template>
 
@@ -26,8 +11,16 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
+      menu: [],
       msg: 'Welcome to Your Vue.js App'
     }
+  },
+  created () {
+    // for (let i = 0; i < 10; i++) {
+    //   for(let j = 0; j < 2; j++) {
+
+    //   }
+    // }
   }
 }
 </script>
--- src/components/context-menu.vue
+++ src/components/context-menu.vue
@@ -2,7 +2,74 @@
 <div @contextmenu.prevent="contextmenu" class="context-menu">
   <slot></slot>
   <div class="context-menu__content">
-    <div></div>
+    <ul>
+      <li class="context-menu--item has-child">
+        <div class="context-menu--item__inner">
+          div
+        </div>
+        <div class="context-menu__content">
+          <ul>
+            <li class="context-menu--item">
+              <div class="context-menu--item__inner">
+                div
+              </div>
+              <div class="context-menu__content">
+                <ul>
+                  <li class="context-menu--item">
+                    <div class="context-menu--item__inner">asdasdasd</div>      
+                  </li>
+                  <li class="context-menu--item">
+                    <div class="context-menu--item__inner">asdasdasd</div>      
+                  </li>
+                  <li class="context-menu--item">
+                    <div class="context-menu--item__inner">asdasdasd</div>      
+                  </li>
+                  <li class="context-menu--item">
+                    <div class="context-menu--item__inner">asdasdasd</div>      
+                  </li>
+                   <li class="context-menu--item">
+                    <div class="context-menu--item__inner">asdasdasd</div>      
+                  </li>
+                  <li class="context-menu--item">
+                    <div class="context-menu--item__inner">asdasdasd</div>      
+                  </li>
+                   <li class="context-menu--item">
+                    <div class="context-menu--item__inner">asdasdasd</div>      
+                  </li>
+                  <li class="context-menu--item">
+                    <div class="context-menu--item__inner">asdasdasd</div>      
+                  </li>
+                </ul>
+              </div>
+            </li>
+            <li class="context-menu--item">
+              <div class="context-menu--item__inner">asdasdasd</div>
+            </li>
+            <li class="context-menu--item">
+              <div class="context-menu--item__inner">asdasdasda</div>
+            </li>
+            <li class="context-menu--item">
+              <div class="context-menu--item__inner">asdasdasd</div>
+            </li>
+            <li class="context-menu--item">
+              <div class="context-menu--item__inner">asdasdasd</div>
+            </li>
+          </ul>
+        </div>
+      </li>
+      <li class="context-menu--item">
+        <div class="context-menu--item__inner">asdasdasd</div>
+      </li>
+      <li class="context-menu--item">
+        <div class="context-menu--item__inner">asdasdasda</div>
+      </li>
+      <li class="context-menu--item">
+        <div class="context-menu--item__inner">asdasdasd</div>
+      </li>
+      <li class="context-menu--item">
+        <div class="context-menu--item__inner">asdasdasd</div>
+      </li>
+    </ul>
   </div>
 </div>
 </template>
@@ -43,11 +110,67 @@ export default {
 }
 .context-menu__content {
   position: absolute;
-  width: 200px;
+  width: 180px;
   min-height: 30px;
-  border: 1px solid #ddd;
+  background: #ffffff;
+  box-shadow: 0 2px 8px #a8a8a8;
+  z-index: 1;
+  ul {
+    list-style: none;
+    padding-left: 0;
+    margin: 0;
+    text-align: left;
+    
+  }
+  .context-menu--item {
+    position: relative;
+    display: block;
+    padding: 0 10px;
+    font-size: 12px;
+    &:hover {
+      background:#efefef;
+    }
+    .context-menu__content {
+      display: none;
+      left: 100%;
+      top: 100%;
+      margin-top: -10px;
+      margin-left: -10px;
+    }
+    &.is-hover,
+    &:hover {
+     >.context-menu__content {
+        display: block;
+      } 
+    }
+  }
 }
-.context-menu__item {
+
+.context-menu--item {
+  &.has-child {
+    >.context-menu--item__inner {
+      &:after {
+        content: " ";
+        position: absolute;
+        right: 0;
+        top: 50%;
+        margin-top: -4px;
+        display: block;
+        width: 0;
+        height: 0;
+        border-style: solid;
+        border-width: 4px;
+        border-color: transparent transparent transparent  #333333;
+      }
+    }
+  }
+}
+
+.context-menu--item__inner {
+  position: relative;
   width: 100%;
+  height: 35px;
+  line-height: 35px;
+  cursor: pointer;
 }
 </style>
